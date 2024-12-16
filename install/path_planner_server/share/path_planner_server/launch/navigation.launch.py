053c2981edb83b837b7c6e8a6f7d19d2ec22a503
Paulo Santos <pcanelasdossantos@gmail.com>
2024-12-16 15:00:00 +0000

    Added files

--- install/path_planner_server/share/path_planner_server/launch/navigation.launch.py
+++ install/path_planner_server/share/path_planner_server/launch/navigation.launch.py
@@ -23,6 +23,12 @@ def generate_launch_description():
     rviz_config_file_path = os.path.join(get_package_share_directory(
         'path_planner_server'), 'rviz_config', 'pathplanning.rviz')
 
+    waypoint_follower_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'waypoint_follower.yaml')
+    
+    filters_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'filters.yaml')
+    
     return LaunchDescription([
         Node(
             package='nav2_map_server',
@@ -77,6 +83,29 @@ def generate_launch_description():
             arguments=['-d', rviz_config_file_path]),
 
         Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            parameters=[waypoint_follower_yaml]),
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='filter_mask_server',
+            output='screen',
+            emulate_tty=True,
+            parameters=[filters_yaml]),
+
+        Node(
+            package='nav2_map_server',
+            executable='costmap_filter_info_server',
+            name='costmap_filter_info_server',
+            output='screen',
+            emulate_tty=True,
+            parameters=[filters_yaml]),
+
+        Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager',
@@ -87,5 +116,8 @@ def generate_launch_description():
                                         'controller_server',
                                         'planner_server',
                                         'recoveries_server',
-                                        'bt_navigator']}])
+                                        'bt_navigator',
+                                        'waypoint_follower',
+                                        'filter_mask_server',
+                                        'costmap_filter_info_server']}])
     ])
